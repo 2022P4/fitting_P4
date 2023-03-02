448422d6511609583a60b46f74ba19476f004426
espri100 <espri100@espri100.local>
2023-03-02 17:00:46 +0900

    Update tdc_RF.C tdc_fit.C

--- tdc_RF.C
+++ tdc_RF.C
@@ -1,6 +1,7 @@
 /*
         qdcを用いたn/gammaの識別
         rootfileを立ち上げる
+         withoutPS ver.
 */
 #include <stdlib.h>
 
@@ -9,43 +10,61 @@
 
 int odd_even(int number);
 
-int sum(int RUNNUMBER) {
+int tdc_RF(int RUNNUMBER) {
     gROOT->Reset();
     gStyle->SetOptStat(1001110);
-    //　本番用
-    // int RUNNUMBER = 150;
+    // 　本番用
     TFile *f = TFile::Open(Form("/mnt/p4root_2022/%04d.root", RUNNUMBER), "R");
-    TFile *g = TFile::Open(Form("./%04d_new.root", RUNNUMBER), "recreate");
+    TFile *g = TFile::Open(Form("./root/%04d_RF.root", RUNNUMBER), "recreate");
+    // test
+    // int RUNNUMBER = 150;
+    // TFile *f = TFile::Open("./sample.root", "R");
+    // TFile *g = TFile::Open("./sample_new.root", "recreate");
 
     /*	wada 用
     TFile *f = TFile::Open("./../../root/0070.root","R");
     TFile *g = TFile::Open("./0070_new.root","recreate");
     */
-    double qdc[13];
-    double hiritsu[13];
+    double qdc[16];
+    double hiritsu[16];
     // 0 + 2*6 ch
     int qdcraw[16];  // qdcrawのデータの箱
     // add 6ch
-    int evenum;         //イベントNo.の管理
-    int IsNeutron[13];  // n/gammaのタグ付け、n=1,gamma=0
-    int ch[13], ch_s;
-    int sCh_qdc[13], sCh_q;
-    int sCh_tdc[7], sCh_t;
+    int evenum;         // イベントNo.の管理
+    int IsNeutron[16];  // n/gammaのタグ付け、n=1,gamma=0
+    int ch[16], ch_s;
+    int sCh_qdc[16], sCh_q;
+    int sCh_tdc[9], sCh_t;
 
-    double factor[13];                //[7]で十分
-    double pedestal[13], pedestal_s;  //ぺデスタル
-    double base[13], base_s;          // n/gammaの識別の基準
+    double factor[16];                //[7]で十分
+    double pedestal[16], pedestal_s;  // ぺデスタル
+    double base[16], base_s;          // n/gammaの識別の基準
 
     int tdcraw[16];
     // int sCh[7], sCh_s;
-    int tdc[7];
-    Double_t tdcsec[7];
-    Double_t tdcsa[7];
+    int tdc[16];
+    int IsHit[9];
+    Double_t tdcsec[9];
+    Double_t tdcsa_FA[9];
+    Double_t tdcsa_SA[9];
 
     int tdc_ch;
-    double slope[7], slope_s, intercept[7], inter_s;
+    double slope[9], slope_s, intercept[9], inter_s;
 
-    //読み込むプログラム-------------------------------------------------
+    for (int i = 0; i < 16; i++) {
+        qdc[i] = 0;
+        hiritsu[i] = 0;
+        IsNeutron[i] = 0;
+        sCh_qdc[i] = 0;
+    }
+    for (int i = 0; i < 9; i++) {
+        sCh_tdc[i] = 0;
+        IsHit[i] = 0;
+        tdcsec[i] = 0;
+        tdcsa_FA[i] = 0;
+        tdcsa_SA[i] = 0;
+    }
+    // 読み込むプログラム-------------------------------------------------
     FILE *fp;
     char fname1[] = "./input/qdc_pedestal.dat";
     fp = fopen(fname1, "r");
@@ -55,13 +74,13 @@ int sum(int RUNNUMBER) {
     fgets(header1, N, fp);  // headerの読み込み
     while (fscanf(fp, "%d%d%lf%lf", &qdc_ch, &ch_s, &pedestal_s, &base_s) !=
            EOF) {
-        //詰め込み
+        // 詰め込み
         ch[qdc_ch] = ch_s;
         pedestal[qdc_ch] = pedestal_s;
         base[qdc_ch] = base_s;
-        //確認
-        // printf("%d %d %f
-        // %f\n",qdc_ch,ch[qdc_ch],pedestal[qdc_ch],base[qdc_ch]);
+        // 確認
+        //  printf("%d %d %f
+        //  %f\n",qdc_ch,ch[qdc_ch],pedestal[qdc_ch],base[qdc_ch]);
     }
     fclose(fp);
 
@@ -72,17 +91,18 @@ int sum(int RUNNUMBER) {
     fgets(header2, N, fp);  // headerの読み込み
     while (fscanf(fp, "%d%d%lf%lf", &tdc_ch, &sCh_t, &slope_s, &inter_s) !=
            EOF) {
-        //詰め込み
-        // ch1-6 is  sCh 1-6, RF is sCh0
+        // 詰め込み
+        //  ch1-6 is  sCh 1-6, RF is sCh0
         slope[sCh_t] = slope_s;
         intercept[sCh_t] = inter_s;
-        //確認
+        // 確認
         printf("%d %f %f\n", tdc_ch, slope[sCh_t], intercept[sCh_t]);
     }
     fclose(fp);
 
     /*----------------------------------------------------------------*/
-
+    /*----- tree setting ---------------------------------------------*/
+    /*----------------------------------------------------------------*/
     TTree *oldtree = (TTree *)f->Get("tree");
     // deifine oldtree
     oldtree->SetBranchAddress("qdcraw", qdcraw);
@@ -91,51 +111,103 @@ int sum(int RUNNUMBER) {
     TTree *tree = new TTree("tree", "tree");
     /* Branchの設定 */
     // qdc area
-    tree->Branch("qdc", qdc, "qdc[13]/D");
-    tree->Branch("sCh_qdc", sCh_qdc, "sCh_qdc[13]/I");
-    tree->Branch("hiritsu", hiritsu, "hiritsu[13]/D");
-    tree->Branch("IsNeutron", IsNeutron, "IsNeutron[13]/I");
+    tree->Branch("qdc", qdc, "qdc[16]/D");
+    tree->Branch("sCh_qdc", sCh_qdc, "sCh_qdc[16]/I");
+    tree->Branch("hiritsu", hiritsu, "hiritsu[16]/D");
+    tree->Branch("IsNeutron", IsNeutron, "IsNeutron[16]/I");
     // tdc area
-    tree->Branch("sCh_tdc", sCh_tdc, "sCh_tdc[7]/I");
-    tree->Branch("tdc", tdc, "tdc[7]/I");
-    tree->Branch("tdcsec", tdcsec, "tdcsec[7]/D");
-    tree->Branch("tdcsa", tdcsa, "tdcsa[7]/D");
+    // tree->Branch("sCh_tdc", sCh_tdc, "sCh_tdc[9]/I");
+    tree->Branch("tdc", tdc, "tdc[9]/I");
+    tree->Branch("tdcsec", tdcsec, "tdcsec[9]/D");
+    tree->Branch("IsHit", IsHit, "IsHit[9]/I");
+    tree->Branch("tdcsa_FA", tdcsa_FA, "tdcsa_FA[9]/D");
+    tree->Branch("tdcsa_SA", tdcsa_SA, "tdcsa_SA[9]/D");
 
     int LOOP = oldtree->GetEntries();
     int e_ch, o_ch;
     /*	データの詰め込み　*/
     for (int i = 0; i < LOOP; i++) {
         oldtree->GetEntry(i);
+        /* data is changeable PS */
+        // To Rewrite
+        // if (qdcraw[15] > 10 || tdcraw[0] > 10)
+        //{
+        //    continue;
+        //}
         evenum = i;
         for (ch_s = 1; ch_s < 7; ch_s++) {
             e_ch = 2 * ch_s;
             o_ch = 2 * ch_s - 1;
             // sinti_ch to qdc_Ch
-            factor[ch_s] = ((double)qdcraw[e_ch] - pedestal[e_ch]) /
-                           ((double)qdcraw[o_ch] - pedestal[o_ch]);
+            switch (ch_s) {
+                case 1:
+                    factor[ch_s] = ((double)qdcraw[2] - pedestal[2]) /
+                                   ((double)qdcraw[1] - pedestal[1]);
+                    break;
+                case 2:
+                    factor[ch_s] = ((double)qdcraw[4] - pedestal[4]) /
+                                   ((double)qdcraw[3] - pedestal[3]);
+                    break;
+                case 3:
+                    factor[ch_s] = ((double)qdcraw[8] - pedestal[8]) /
+                                   ((double)qdcraw[5] - pedestal[5]);
+                    break;
+                case 4:
+                    factor[ch_s] = ((double)qdcraw[10] - pedestal[10]) /
+                                   ((double)qdcraw[9] - pedestal[9]);
+                    break;
+                case 5:
+                    factor[ch_s] = ((double)qdcraw[12] - pedestal[12]) /
+                                   ((double)qdcraw[11] - pedestal[11]);
+                    break;
+                case 6:
+                    factor[ch_s] = ((double)qdcraw[14] - pedestal[14]) /
+                                   ((double)qdcraw[13] - pedestal[13]);
+                    break;
+                case 0:
+                    factor[0] = 0;
+                    break;
+            }
+            // rewrite factor
+            //
+            // factor[ch_s] = ((double)qdcraw[e_ch] - pedestal[e_ch]) /
+            //               ((double)qdcraw[o_ch] - pedestal[o_ch]);
             // fast/allのパラメータ
         }
-        factor[0] = 0;
-        // [0] is not used
-        for (int j = 0; j < 13; j++) {
+
+        for (int j = 0; j < 16; j++) {
             qdc[j] = qdcraw[j] - pedestal[j];
             // pedestalの分を差し引いたqdcの値
-            if (j < 7) {
+            if (j < 7 && j > 0) {
                 int j_odd = 2 * j - 1;
+                switch (j) {
+                    case 4:
+                        j_odd = 9;
+                        break;
+                    case 5:
+                        j_odd = 11;
+                        break;
+                    case 6:
+                        j_odd = 13;
+                        break;
+                    default:
+                        j_odd = 2 * j - 1;
+                        break;
+                }
                 sCh_qdc[j] = j;
                 hiritsu[j] = factor[j];
                 if (hiritsu[j] > base[j] &&
                     qdcraw[j_odd] - pedestal[j_odd] >
-                        500) {  // n/gamma && minimum energy
+                        200) {  // n/gamma && minimum energy
                     IsNeutron[j] = j;
                 } else {
                     IsNeutron[j] = 0;
                 }
             } else {
                 // for j>7
-                sCh_qdc[j] = 100;
+                sCh_qdc[j] = -1;
                 hiritsu[j] = 0;
-                IsNeutron[j] = 7;
+                IsNeutron[j] = 0;
             }
             /*
                 // sintiの番号をtdcに対応させる
@@ -149,16 +221,57 @@ int sum(int RUNNUMBER) {
             */
         }
         // for tdc
-        for (int j = 0; j < 7; j++) {
-            sCh_t = (j + 1) % 7;
+        for (int j = 0; j < 9; j++) {
+            // sCh_t = (j + 1) % 7;
+            sCh_t = j;
             // if (i == 1) cout << sCh_t << endl;
             sCh_tdc[sCh_t] = sCh_t;
             tdc[sCh_t] = tdcraw[j];
+
             // sintiの番号を対応させる
-            tdcsec[sCh_t] = slope[sCh_t] * (double)tdcraw[j] + intercept[sCh_t];
-            // ch->time[ns]
-            tdcsa[sCh_t] = tdcsec[0] - tdcsec[sCh_t];
-            // RF(ch0)-tdcsec [ns]
+            // tdcsec[sCh_t] = slope[sCh_t] * (double)tdcraw[j] +
+            // intercept[sCh_t];
+            tdcsec[sCh_t] =
+                ((double)tdcraw[j] + intercept[sCh_t]) / slope[sCh_t];
+            // y = ax+b -> y = (x+b)/a
+        }
+        for (int j = 0; j < 9; j++) {
+            sCh_t = j;
+
+            // TO REWRITE
+             if ((tdcsec[j] < 59 && tdcsec[j] > 54)|| j==7 || j == 8){
+                 // ch->time[ns]
+                tdcsa_FA[sCh_t] = (double)(tdcsec[7] - tdcsec[sCh_t]);
+                IsHit[sCh_t] = sCh_t;
+                // RF(ch7)-tdcsec [ns]
+                if (tdcsa_FA[sCh_t] < 0) {
+                    tdcsa_FA[sCh_t] = 0.;
+                }
+                tdcsa_SA[sCh_t] = (double)tdcsec[8] - tdcsec[sCh_t];
+                if (tdcsa_SA[sCh_t] < 0) {
+                    tdcsa_SA[sCh_t] = 0.;
+                }
+                // RF(ch8)-tdcsec [ns]
+                // tdc_sa is minus, data vanish
+             }/* else if((tdcsec[j] < 18 && tdcsec[j] > 14) || j == 7 || j == 8) {
+                // ch->time[ns]
+                tdcsa_FA[sCh_t] = (double)(tdcsec[7] - tdcsec[sCh_t]);
+                IsHit[sCh_t] = sCh_t;
+                // RF(ch7)-tdcsec [ns]
+                if (tdcsa_FA[sCh_t] < 0) {
+                    tdcsa_FA[sCh_t] = 0.;
+                }
+                tdcsa_SA[sCh_t] = (double)tdcsec[8] - tdcsec[sCh_t];
+                if (tdcsa_SA[sCh_t] < 0) {
+                    tdcsa_SA[sCh_t] = 0.;
+                }
+                // RF(ch8)-tdcsec [ns]
+                // tdc_sa is minus, data vanish
+            } */else {
+                tdcsa_FA[sCh_t] = -1.;
+                tdcsa_SA[sCh_t] = -1.;
+                IsHit[sCh_t] = 0.;
+            }
         }
         tree->Fill();
     }
@@ -166,16 +279,16 @@ int sum(int RUNNUMBER) {
 
     /*	test */
     // tree->Draw("qdc[1]:qdc[0]>>hadc0(1000,0,4000,1000,0,2000)","IsNeutron==1");
-    printf("neutron count is %lld\n", tree->GetEntries("IsNeutron!=0"));
+    printf("neutron count is %lld\n", tree->GetEntries("IsNeutron==1"));
     printf("all count is %lld\n", tree->GetEntries(""));
 
     return 0124;
 }
 //	define the function
 int odd_even(int number) {
-    if (number % 2 == 0) {  //偶数なら/2したもの
+    if (number % 2 == 0) {  // 偶数なら/2したもの
         return number / 2;
-    } else {  //偶数なら(+1)/2したもの
+    } else {  // 偶数なら(+1)/2したもの
         return (1 + number) / 2;
     }
 }
--- tdc_fit.C
+++ tdc_fit.C
@@ -8,111 +8,203 @@
 #include <iostream>
 #define N 256
 #define Mn 939.56
-#define L 33.0         //[m]
+#define L 39.912       //[m]
 #define c 0.299792458  //[m/ns]
 
-double Ene(double Ene);
+double Ene(double time);
+double Beta(double time);
 
 void tdc_fit(int RUNNUMBER) {
+    // int RUNNUMBER = 115;
     gROOT->Reset();
     gStyle->SetOptStat(1001110);
-    //　本番用
-    // int RUNNUMBER = 150;
-    TFile *f =
-        TFile::Open(Form("/mnt/p4root_2022/%04d_new.root", RUNNUMBER), "R");
-    TFile *g = TFile::Open(Form("./%04d_new2.root", RUNNUMBER), "recreate");
+    // 　本番用
+    //  int RUNNUMBER = 150;
 
+    TFile *f = TFile::Open(Form("./root/%04d_RF.root", RUNNUMBER), "R");
+    TFile *g = TFile::Open(Form("./root/%04d_fit.root", RUNNUMBER),"recreate");
+    //  sample test
+    //TFile *f = TFile::Open("./sample_new.root", "R");
+    //TFile *g = TFile::Open("./sample_new2.root", "recreate");
     /*	wada 用
     TFile *f = TFile::Open("./../../root/0070.root","R");
     TFile *g = TFile::Open("./0070_new.root","recreate");
     */
 
-    double IsNeutron[13];
-    double tdcsa[7];
+    int IsNeutron[16];
+    int IsHit[9];
+    double tdcsa_FA[9];
+    double tdcsa_SA[9];
     // new area
-    double tof_g = 100;  //[ns]
+    double tof_g = L/c;  //[ns]
     // tdc_gamma - tdc_n
-    double tdc_gn[7];
+    double tdc_gn_FA[7];
     double trf = 60;  //[ns]
-    double ToFnraw_3[7];
-    double Tn_3[7];
-    double ToFnraw_4[7];
-    double Tn_4[7];
-    int Num = 0;
+    double ToFnraw_3_FA[7];
+    double Tn_3_FA[7];
 
+    double tdc_gn_SA[7];
+    double ToFnraw_3_SA[7];
+    double Tn_3_SA[7];
+    double beta_FA[7];
+    double beta_SA[7];
+    // double ToFnraw_4[7];
+    // double Tn_4[7];
+    int Num = 0;
+    // reset the data
+    for (int i = 0; i < 7; i++) {
+        tdcsa_FA[i] = 0.;
+        tdc_gn_FA[i] = 0.;
+        tdcsa_SA[i] = 0.;
+        tdc_gn_SA[i] = 0.;
+        ToFnraw_3_FA[i] = -1.;
+        ToFnraw_3_SA[i] = -1.;
+        beta_FA[i] = -1.;
+        beta_SA[i] = -1.;
+        Tn_3_SA[i] = -1;
+        Tn_3_FA[i] = -1;
+    }
     TTree *oldtree = (TTree *)f->Get("tree");
 
-    oldtree->SetBarnchAddress("IsNeutron", IsNeutron);
-    oldtree->SetBarnchAddress("tdcsa", tdcsa);
+    oldtree->SetBranchAddress("IsNeutron", IsNeutron);
+    oldtree->SetBranchAddress("IsHit", IsHit);
+    oldtree->SetBranchAddress("tdcsa_FA", tdcsa_FA);
+    oldtree->SetBranchAddress("tdcsa_SA", tdcsa_SA);
 
     TTree *tree = new TTree("tree", "tree");
-    // old area
-    tree->Branch("IsNeutron", IsNeutron, "IsNeutron[13]/I");
-    tree->Barnch("tdcasa", tdcsa, "tdcsa[7]/D");
-    // new area
-    tree->Barnch("tdc_gn", tdc_gn, "tdc_gn[7]/D");
-    tree->Branch("ToFnraw", ToFnraw, "ToFnraw[7]/D");
-    tree->Branch("Tn", Tn, "Tn[7]/D");
+    TTree *Etree = new TTree("Etree", "Etree");
+    // old area =================================================//
+    tree->Branch("IsNeutron", IsNeutron, "IsNeutron[7]/I");
+    tree->Branch("IsHit", IsHit, "IsHit[7]/I");
+    tree->Branch("tdcsa_FA", tdcsa_FA, "tdcsa_FA[7]/D");
+    tree->Branch("tdcsa_SA", tdcsa_SA, "tdcsa_SA[7]/D");
+    // new area =================================================//
+    tree->Branch("tdc_gn_FA", tdc_gn_FA, "tdc_gn_FA[7]/D");
+    tree->Branch("ToFnraw_3_FA", ToFnraw_3_FA, "ToFnraw_3_FA[7]/D");
+    tree->Branch("Tn_3_FA", Tn_3_FA, "Tn_3_FA[7]/D");
 
-    TH1 *oldhist[7];  // [0] is not used
+    tree->Branch("tdc_gn_SA", tdc_gn_SA, "tdc_gn_SA[7]/D");
+    tree->Branch("ToFnraw_3_SA", ToFnraw_3_SA, "ToFnraw_3_SA[7]/D");
+    tree->Branch("Tn_3_SA", Tn_3_SA, "Tn_3_SA[7]/D");
+    // Etree
+    Etree->Branch("IsNeutron", IsNeutron, "IsNeutron[7]/I");
+    Etree->Branch("IsHit", IsHit, "IsHit[7]/I");
+    Etree->Branch("beta_FA", beta_FA, "beta_FA[7]/D");
+    Etree->Branch("Tn_3_FA", Tn_3_FA, "Tn_3_FA[7]/D");
+    Etree->Branch("beta_SA", beta_SA, "beta_SA[7]/D");
+    Etree->Branch("Tn_3_SA", Tn_3_SA, "Tn_3_SA[7]/D");
 
-    Double_t MINCH = 0;
-    Double_t MAXCH = 0;
-
+    /* fitting area FA------------------------------------------------------*/
+    TH1 *oldhist_FA[7];  // [0] is not used
+    double MINCH = -2000.;
+    double MAXCH = 2000.;
+    // make the histgram
     for (int i = 1; i < 7; i++) {
-        oldhist[i] = new TH1F(
-            Form("tdc_sa %3d_sinti ch%d", RUNNUMBER, i),
+        oldhist_FA[i] = new TH1F(
+            Form("tdc_sa_FA %3d_sinti ch%d", RUNNUMBER, i),
             Form("#%3d double  gamma gaus fitting qdc ch%d;tdcsa[ns];Count",
                  RUNNUMBER, i),
-            4096, MINCH, MAXCH);
+            1000, MINCH, MAXCH);
     }
-
+    // fill histgram
     for (int i = 1; i < 7; i++) {
-        oldtree->Draw(Form("qdcsa[%d]>>tdc_sa %3d_sinti ch%d", i, RUNNUMBER, i),
-                      Form("IsNeutron[%d]==0", i));
+        oldtree->Draw(
+            Form("tdcsa_FA[%d]>>tdc_sa_FA %3d_sinti ch%d", i, RUNNUMBER, i),
+            Form("IsNeutron[%d]==0", i));
     }
-
-    double init_mu[7];
-    init_mu1[0] = 0;
+    // set fitting parameter
+    // 変更があるならこの場所
+    // ---------------------------------------------------------------------------------------//
+    double init_mu_FA[7];
+    init_mu_FA[0] = 0;
     for (int ch = 1; ch < 7; ch++) {
-        init_mu[ch] = 110.;
+        init_mu_FA[ch] = 30.;
     }
-
-    double range = 100.;
-
+    double range = 10.;
+    // ---------------------------------------------------------------------------------------//
+    // make gaus+constant
     TF1 *gaus111[7];
     for (int i = 1; i < 7; i++) {
         gaus111[i] = new TF1(Form("gaus%3d_%d", RUNNUMBER, i),
-                             "[0]*exp(-0.5*pow((x-[1])/"
-                             "[2],2.0))+[4]",
-                             init_mu[i] - range, init_mu[i] + range);
+                             "[0]*exp(-0.5*pow((x-[1])/[2],2.0))+[4]",
+                             init_mu_FA[i] - range, init_mu_FA[i] + range);
 
-        gaus111[i]->SetParameters(1000., init_mu[i], 50., 10.);
+        gaus111[i]->SetParameters(1000., init_mu_FA[i], 5., 100.);
         gaus111[i]->SetNpx(1000);
     }
 
+    // fit
     for (int i = 1; i < 7; i++) {
-        oldhist[i]->Fit(gaus111[i], "NRQ");
+        oldhist_FA[i]->Fit(gaus111[i], "NRQ");
     }
-
+    // get the parameter
     double height[7];
-    double mean[7];
+    double mean_FA[7];
+    double mean_SA[7];
     double sigma[7];
-    double const[7];
+    double constant[7];
 
     for (int i = 1; i < 7; i++) {
         height[i] = gaus111[i]->GetParameter(0);
-        mean[i] = gaus111[i]->GetParameter(1);
+        mean_FA[i] = gaus111[i]->GetParameter(1);
         sigma[i] = gaus111[i]->GetParameter(2);
-        const[i] = gaus111[i]->GetParameter(3);
+        constant[i] = gaus111[i]->GetParameter(3);
+
+        printf("mean_FA%d = %f\n", i, mean_FA[i]);
     }
-    /* test area -------------------------------------*/
-    test = 1;
-    printf("#%04d_ch%d\n", RUNNUMBER, test);
-    cout << "height = " << height[test] << endl;
-    cout << "mean = " << mean[test] << endl;
-    cout << "sigma = " << sigma[test] << endl;
-    cout << "const = " << const[test] << endl;
+    /* fitting area SA ------------------------------------------------------*/
+    TH1 *oldhist_SA[7];  // [0] is not used
+    MINCH = -2000.;
+    MAXCH = 2000.;
+    // make the histgram
+    for (int i = 1; i < 7; i++) {
+        oldhist_FA[i] = new TH1F(
+            Form("tdc_sa_SA %3d_sinti ch%d", RUNNUMBER, i),
+            Form("#%3d double  gamma gaus fitting qdc ch%d;tdcsa[ns];Count",
+                 RUNNUMBER, i),
+            1000, MINCH, MAXCH);
+    }
+    // fill histgram
+    for (int i = 1; i < 7; i++) {
+        oldtree->Draw(
+            Form("tdcsa_SA[%d]>>tdc_sa_SA %3d_sinti ch%d", i, RUNNUMBER, i),
+            Form("IsNeutron[%d]==0 && tdcsa_SA[%d] > 0", i, i));
+    }
+    // set fitting parameter
+    // 変更があるならこの場所
+    // ---------------------------------------------------------------------------------------//
+    double init_mu_SA[7];
+    init_mu_SA[0] = 0;
+    for (int ch = 1; ch < 7; ch++) {
+        init_mu_SA[ch] = 90.;
+    }
+    // init_mu_SA[5] = 60;
+    //  ---------------------------------------------------------------------------------------//
+    //  make gaus+constant
+    //  TF1 *gaus111[7];
+    for (int i = 1; i < 7; i++) {
+        gaus111[i] = new TF1(Form("gaus%3d_%d", RUNNUMBER, i),
+                             "[0]*exp(-0.5*pow((x-[1])/[2],2.0))+[4]",
+                             init_mu_SA[i] - range, init_mu_SA[i] + range);
+
+        gaus111[i]->SetParameters(1000., init_mu_SA[i], 5., 100.);
+        gaus111[i]->SetNpx(1000);
+    }
+
+    // fit
+    for (int i = 1; i < 7; i++) {
+        oldhist_FA[i]->Fit(gaus111[i], "NRQ");
+    }
+
+    for (int i = 1; i < 7; i++) {
+        height[i] = gaus111[i]->GetParameter(0);
+        mean_SA[i] = gaus111[i]->GetParameter(1);
+        sigma[i] = gaus111[i]->GetParameter(2);
+        constant[i] = gaus111[i]->GetParameter(3);
+
+        printf("mean_SA%d = %f\n", i, mean_SA[i]);
+    }
+
     /*--------------------------------------------- */
     int LOOP = oldtree->GetEntries();
     int evenum;
@@ -121,28 +213,46 @@ void tdc_fit(int RUNNUMBER) {
         evenum = i;
 
         for (int j = 0; j < 7; j++) {
-            // 
+            //
             IsNeutron[j] = IsNeutron[j];
-            // tdc_gamma-neutron [ns]
-            tdc_gn[j] = mean[j] - tdcsa[j];
-            Num = 3;
-            ToFnraw_3[j] = tof_g + tdc_gn[j] + Num * trf;
-            // Tn3
-            Tn_3[j] = Ene(ToFnraw_3[j]);
-            if (Tn_3[j] < 0 && Tn_3[j] > 70) {
-                Tn_3[j] = 0.;
+            IsHit[j] = IsHit[j];
+            if (j == 0 || IsHit[j] == 0) {
+                continue;
             }
-            // Tn4 
+            // tdc_gamma-neutron [ns]
+            tdc_gn_FA[j] = mean_FA[j] - tdcsa_FA[j];
+            tdc_gn_SA[j] = mean_SA[j] - tdcsa_SA[j];
+            Num = 4;
+// original is 3
+            ToFnraw_3_FA[j] = tof_g + tdc_gn_FA[j] + Num * trf;
+            ToFnraw_3_SA[j] = tof_g + tdc_gn_SA[j] + Num * trf;
+            // Tn3 N= 3 Energy[MeV]
+            beta_FA[j] = Beta(ToFnraw_3_FA[j]);
+            beta_SA[j] = Beta(ToFnraw_3_SA[j]);
+            Tn_3_FA[j] = Ene(ToFnraw_3_FA[j]);
+            Tn_3_SA[j] = Ene(ToFnraw_3_SA[j]);
+            if (Tn_3_FA[j] < 0) {
+                Tn_3_FA[j] = 0.;
+            }
+            if (Tn_3_SA[j] < 0) {
+                Tn_3_SA[j] = 0.;
+            }
+            // Tn4
+            /*
             Num = 4;
             ToFnraw_4[j] = tof_g + tdc_gn[j] + Num * trf;
             Tn_4[j] = Ene(ToFnraw_4[j]);
-            if (Tn_4[j] < 0 && Tn_4[j] > 70) {
+            if (Tn_4[j] < 0 && Tn_4[j] > 70)
+            {
                 Tn_4[j] = 0.;
             }
+            */
         }
         tree->Fill();
+        Etree->Fill();
     }
-    tree->write();
+    tree->Write();
+    Etree->Write();
 }
 double Ene(double time) {
     double beta = L / (time * c);
@@ -150,3 +260,7 @@ double Ene(double time) {
     double Tn = Mn * (-1.0 + pow(1 - beta2, -0.5));
     return Tn;
 }
+double Beta(double time) {
+    double beta = L / (time * c);
+    return beta;
+};
